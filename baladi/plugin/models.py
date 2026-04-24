879b0aa2d7402fd7ddde0dbfe36fccd3e171b086
Bialy <bcalderonm@gmail.com>
2026-04-24 13:23:59 -0600

    models y views

--- baladi/plugin/models.py
+++ baladi/plugin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+
+class Flux(models.Model):
+	date_time = models.DateTimeField("Date published")
+	flux = models.DecimalField(default = 0.0)
+
--- baladi/plugin/views.py
+++ baladi/plugin/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Flux
+from datetime import datetime
+
 # Create your views here.
 
 def index(request):
+	now = datetime.now()
+	flux = Flux(now, 1.0)
+	flux.save()
 	return HttpsResponse("Este es mi microservicio")
